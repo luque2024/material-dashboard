af6d18b15a2921e40ac003b2dffd5311aec60ae5
Ivan Luque <ivan.luque2016@gmail.com>
2024-10-22 16:21:37 -0400

    inicio del perfil de usuario

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -124,11 +124,11 @@ session_start();
           <h6 class="ps-4 ms-2 text-uppercase text-xs text-white font-weight-bolder opacity-8">Account pages</h6>
         </li>
         <li class="nav-item">
-          <a class="nav-link text-white " href="../pages/profile.html">
+          <a class="nav-link text-white " href="../pages/profile.php">
             <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
               <i class="material-icons opacity-10">person</i>
             </div>
-            <span class="nav-link-text ms-1">Profile</span>
+            <span class="nav-link-text ms-1">Perfil</span>
           </a>
         </li>
         <li class="nav-item">
@@ -187,6 +187,10 @@ session_start();
         </div>
       </div>
     </nav>
+
+
+
+
 
   <body>
    
--- pages/profile.php
+++ pages/profile.php
@@ -1,8 +1,5 @@
-
 <?php
-if (session_status() == PHP_SESSION_NONE) {
-    session_start();
-}
+session_start();
 
 // Verificar si el usuario ha iniciado sesión
 if (!isset($_SESSION['user_id'])) {
